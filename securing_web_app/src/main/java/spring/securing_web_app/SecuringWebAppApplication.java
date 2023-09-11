3dbe42459efd299d620458f0dc76f7e6268d74fc
MirzaAbdullahAlNoman <mirza.abdullah.noman@gmail.com>
2023-09-12 03:38:24 +0600

    Initial commit

---
+++ securing_web_app/src/main/java/spring/securing_web_app/SecuringWebAppApplication.java
@@ -0,0 +1,13 @@
+package spring.securing_web_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuringWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuringWebAppApplication.class, args);
+	}
+
+}
